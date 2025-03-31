3d83a7f0aa34e2d3bd07cb02e00c6d8bb1627dff
tbk81 <dev@tbk81.com>
2025-03-31 06:13:54 -0700

    profile-maker

---
+++ charSheet/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	return
+}
+
+type travelerProfile struct {
+	name string
+	// origin    string
+	// homeworld string
+	// archtype  string
+	// class     string
+	// age       int
+	// consLevel int
+}
+
+func travelerMaker(t travelerProfile) {
+	traveler :=
+		fmt.Println("Type name: ")
+	fmt.Scanln(&t.name)
+}
+
+/*
+Character sheet for Exodus TTRPG
+*/
